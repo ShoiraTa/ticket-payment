457eef03de062417705d0c75ad6da536e281947f
shoiraTa <shoira.shakirovna@gmail.com>
2022-07-29 12:38:12 +0400

    specs: TicketReservation service  spec

--- spec/services/ticket_reservation_spec.rb
+++ spec/services/ticket_reservation_spec.rb
@@ -1,36 +1,28 @@
 # frozen_string_literal: true
-# # frozen_string_literal: true
 
-# RSpec.describe TicketPayment do
-#   describe ".call" do
-#     before do
-#       @ticket = Ticket.create(event: event, available: 10, price: 100)
-#       @ticket.reservations.create(user_id: user_id, tickets_count: tickets_count )
-#     end
+RSpec.describe TicketReservation do
+  describe ".call" do
+    let(:event) { create(:event, :with_ticket) }
+    let(:ticket) { create(:ticket, :with_reservation) }
+    let(:user_id) { 1 }
+    let(:tickets_count) { 1 }
 
-#     subject { described_class.call(user_id, token, event) }
-#     let(:event) { create(:event, :with_ticket) }
-#     let(:token) { "token" }
-#     let(:user_id) { 1 }
-#     let(:tickets_count) { 2 }
+    subject { described_class.call(ticket, tickets_count) }
+    context "when tickets are available" do
+      it "should update available tickets count" do
+        expect { subject }.to change(ticket, :available).by(-1)
+      end
 
-#     context "when tickets are available" do
-#       it "should call payment adapter" do
-#         expect(Payment::Gateway).to receive(:charge).with(amount: (@ticket.price * tickets_count), token: token)
-#         subject
-#       end
+      it "should create a reservation" do
+        expect { subject }.to change{Reservation.count}
+      end
+    end
 
-#       # it "should update available tickets count" do
-#       #   expect { subject }.to change(ticket, :available).by(-1)
-#       # end
-#     end
-
-#     # context "when tickets are not available" do
-#     #   let(:tickets_count) { ticket.available + 1 }
-
-#     #   it "should raise error" do
-#     #     expect { subject }.to raise_error(TicketPayment::NotEnoughTicketsError)
-#     #   end
-#     # end
-#   end
-# end
+    context "when tickets are not available" do
+      let(:tickets_count) { ticket.available + 1 }
+      it "should raise error" do
+        expect { subject }.to raise_error(TicketReservation::NotEnoughTicketsError)
+      end
+    end
+  end
+end
